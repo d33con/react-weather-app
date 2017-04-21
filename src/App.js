9714eb67925fbc144afa60eae9f1faa598fb47e6
Oli Bullen <d33con@gmail.com>
2017-04-21 13:35:35 +0900

    => stateless component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GetForecast from './GetForecast';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <GetForecast />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <GetForecast/>
+    </div>
+  );
 }
 
 export default App;
